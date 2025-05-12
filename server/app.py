6a3fe4ddf03bc32b626a4ce1e5f39a7407d22c73
Anne Nabwire <nabanne96@gmail.com>
2025-05-12 22:30:43 +0300

    lab completed

--- server/app.py
+++ server/app.py
@@ -21,6 +21,40 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+
+def pet_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude':earthquake.magnitude,
+                'year':earthquake.year}
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quakes_data = [{
+        'id': eq.id,
+        'location': eq.location,
+        'magnitude': eq.magnitude,
+        'year': eq.year
+    } for eq in earthquakes]
+
+    response = {
+        'count': len(quakes_data),
+        'quakes': quakes_data
+    }
+
+    return make_response(response, 200)
 
 
 if __name__ == '__main__':
